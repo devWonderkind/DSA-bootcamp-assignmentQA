104b799c9a84c866acd0ff1f4675fa122c26c2f6
Aakanksha Chahal <akanshachahal758@gmail.com>
2023-08-02 22:19:47 +0530

    Area of triangle

--- AreaOfTriangle.java
+++ AreaOfTriangle.java
@@ -1,6 +1,13 @@
 import java.util.Scanner;
 public class AreaOfTriangle {
     public static void main(String[] args) {
-
+        float h, b, area;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the height:");
+        h = input.nextFloat();
+        System.out.print("Enter the base:");
+        b = input.nextFloat();
+        area = (h*b)/2;
+        System.out.println("The area of triangle is" + area);
     }
 }
